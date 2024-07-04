8566e16e19edceb50f4beb69dc119a0f7928b758
vule19 <anhvle1901@gmail.com>
2024-07-04 17:13:07 -0400

    register and login

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import random
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template('index.html')
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    return render_template('login.html')
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
 
 ## New Ticket Page
 @app.route("/newticket/")
