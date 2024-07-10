66d2d17c379f6e40bb360537eaaad2d2ee50f32b
Cteklitz <cteklitz@ufl.edu>
2024-07-10 12:45:07 -0400

    Add ticket assign function

--- db.py
+++ db.py
@@ -26,7 +26,7 @@ def create_collections():
         db.create_collection('accounts')
 
 def create_indexes():
-    db.tickets.create_index ({'ticketID' : 1, 'userID' : 1, 'category' : 1, 'description' : 1})
+    db.tickets.create_index ({'ticketID' : 1, 'userID' : 1, 'category' : 1, 'description' : 1, 'assignedEmpID' : 1, 'status' : 1}) # status can either be: 'unassigned' 'assigned' or 'closed'
     db.accounts.create_index ({'accID' : 1,'username' : 1, 'password' : 1, 'fName' : 1, 'lName' : 1})
 
 def init_app(app):
@@ -36,10 +36,15 @@ def init_app(app):
 
 ## Ticket Fucntions
 def new_ticket(ticketID, userID, category, description):
-    ticket_doc = {'ticketID' : ticketID, 'userID' : userID, 'category' : category, 'description' : description}
+    ticket_doc = {'ticketID' : ticketID, 'userID' : userID, 'category' : category, 'description' : description, 'status' : "unassigned"}
     return db.tickets.insert_one(ticket_doc)
 
 def get_ticket_count():
     return db.tickets.count_documents({})
+
+def assign_ticket(ticketID, empID):
+    response = db.tickets.update_one({'ticketID' : ticketID}, {'$set' : {'assignedEmpID' : empID, 'status' : "assigned"}})
+    return response
+
 
 ## Account Functions
