ec4428692cb4d439cd0f3e6b42c069cb6c960bf4
Aidan Doherty <119699042+aidandoherty01@users.noreply.github.com>
2024-07-12 00:34:04 -0400

    Basic Admin Functionality
    Repurposed 'debug.html' and its 'app.py' function into an admin interface.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, redirect, render_template, current_app, g
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+
 import random
 import os
-import configparser
+import datetime
+import string
 
-from db import init_app, new_ticket, get_ticket_count
-
+from db import init_app, new_ticket, get_ticket_count, get_ticket_by_id, get_tickets_by_acc, assign_ticket_emp, assign_ticket_eta, assign_ticket_start_time, close_ticket, new_account, delete_account, get_account_count, get_accounts
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://admin:j6BIXDqwhnSevMT9@group29.xghzavk.mongodb.net/testDB"
@@ -18,20 +19,33 @@ def index():
     return render_template('index.html')
 
 @app.route("/homepage/", methods=["GET", "POST"])
-def hoempage():
+def homepage():
     return render_template('homepage.html')
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
     return render_template('login.html')
 
+# Register for a new account
 @app.route("/register/", methods=["GET", "POST"])
 def register():
-    return render_template('register.html')
+    if (request.method == 'POST'):
+        accID = get_account_count() + 1
+        username = request.form.get("username")
+        password = request.form.get("password")
+        fname = request.form.get("fname")
+        lname = request.form.get('lname')
+
+        new_account(accID, username, password, fname, lname)
+
+        return redirect("/login/")
+    
+    else:
+        return render_template('register.html')
 
 ## New Ticket Page
 @app.route("/newticket/", methods=['GET', 'POST'])
-def newTicket():   
+def newTicket():
     if (request.method == 'POST'):
         out = "" # will hold the info sumbitted by user to be sent to database as a newticket, seperated by commas
         # formatted as: "accID,category,description"
@@ -42,7 +56,7 @@ def newTicket():
         category = request.form['catagories']
         desc = request.form['desc']
 
-        ticketID = get_ticket_count() + 1
+        ticketID = get_ticket_count() + 1   # potential bug if tickets can be deleted, generating new tickets with overlapping ids
 
         new_ticket(ticketID, accID, category, desc) # creates a new ticket with the info given
 
@@ -61,6 +75,48 @@ def ticketSubmitted():
         return redirect("/homepage/") # link to homepage/dashboard/ect goes here
     else:
         return render_template('ticketsubmitted.html')
+    
+## Admin Page
+@app.route("/admin/", methods=["GET", "POST"])
+def admin():
+    if (request.method == 'POST'):
+        if (request.form['submit'] == "assign"):            
+            ticketID = request.form['ticketID']
+            empID = request.form['empID']
+            assign_ticket_emp(ticketID, empID)
+        elif (request.form['submit'] == "close"):
+            ticketID = request.form['ticketIDc']
+            close_ticket(ticketID)
+        elif (request.form['submit'] == "lookTicket"):
+            ticketID = request.form['ticketIDl']
+            ticket = get_ticket_by_id(ticketID)
+            print(ticket)
+        elif (request.form['submit'] == "lookAcc"):
+            accID = request.form['accID']
+            tickets = get_tickets_by_acc(accID)
+            for x in tickets:
+                print(x)
+        # Start Admin Functionality
+        elif (request.form['submit'] == 'allEmp'):  # On button press, display all employee accounts and information
+            accounts = get_accounts()
+            return render_template('admin.html', accounts=accounts)
+        elif (request.form['submit'] == 'createEmp'):
+            accID = get_account_count()
+            fname = request.form['fname']
+            lname = request.form['lname']
+            username = request.form['username']
+            password = request.form['password']
+            new_account(accID, username, password, fname, lname)
+        elif (request.form['submit'] == 'deleteEmp'):
+            accID = request.form['delID']
+            # if not isinstance(accID, int):
+                # print('Error: Incorrect Input')   # Figure out better way to handle this
+            # else:
+                # delete_account(accID)
+            delete_account(int(accID))
+        # add ticket view
+
+    return render_template('admin.html')
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 import bson
+import datetime
+import string
 
 from flask import current_app, g
 from werkzeug.local import LocalProxy
@@ -8,7 +10,8 @@ from bson.objectid import ObjectId
 from bson.errors import InvalidId
 
 
-def get_db():
+
+def get_db():   # Grabs the database and returns as a variable
     db = getattr(g, "_database", None)
 
     if db is None:
@@ -17,7 +20,12 @@ def get_db():
 
     return db
 
-db = LocalProxy(get_db)
+db = LocalProxy(get_db) # Creates a proxy of the bound object, used to make working with it easier
+
+def init_app(app):  # Initializes collections and indexes (if necessary)
+    with app.app_context():
+        create_collections()
+        create_indexes()
 
 def create_collections():
     if 'tickets' not in db.list_collection_names():
@@ -26,20 +34,51 @@ def create_collections():
         db.create_collection('accounts')
 
 def create_indexes():
-    db.tickets.create_index ({'ticketID' : 1, 'userID' : 1, 'category' : 1, 'description' : 1})
+    db.tickets.create_index ({'ticketID' : 1, 'userID' : 1, 'category' : 1, 'description' : 1, 'assignedEmpID' : 1, 'status' : 1, 'eta' : 1, 'startTime' : 1}) # status can either be: 'unassigned' 'assigned' or 'closed'
     db.accounts.create_index ({'accID' : 1,'username' : 1, 'password' : 1, 'fName' : 1, 'lName' : 1})
 
-def init_app(app):
-    with app.app_context():
-        create_collections()
-        create_indexes()
-
-## Ticket Fucntions
+## Ticket Functions
 def new_ticket(ticketID, userID, category, description):
-    ticket_doc = {'ticketID' : ticketID, 'userID' : userID, 'category' : category, 'description' : description}
+    ticket_doc = {'ticketID' : int(ticketID), 'userID' : int(userID), 'category' : category, 'description' : description, 'status' : "unassigned"}
     return db.tickets.insert_one(ticket_doc)
 
 def get_ticket_count():
     return db.tickets.count_documents({})
 
+def get_ticket_by_id(ticketID):
+    return db.tickets.find_one({'ticketID' : int(ticketID)}) # returns the ticket associated with the ID
+
+def get_tickets_by_acc(accID): # returns a cursor that points to the first element in the list of tickets associated with a given account
+    tickets = db.tickets.find({'userID' : int(accID)})
+    if (len(list(tickets)) == 0):
+        tickets = db.tickets.find({'assignedEmpID' : int(accID)})
+    return tickets
+     
+def assign_ticket_emp(ticketID, empID):
+    response = db.tickets.find_one_and_update({'ticketID' : int(ticketID)}, {'$set' : {'assignedEmpID' : int(empID), 'status' : "assigned"}})
+    return response
+
+def assign_ticket_eta(ticketID, eta):
+    response = db.tickets.find_one_and_update({'ticketID' : int(ticketID)},  {'$set' : {'eta' : string(eta)}})
+    return response
+
+def assign_ticket_start_time(ticketID, startTime):
+    response = db.tickets.find_one_and_update({'ticketID' : int(ticketID)}, {'$set' : {'startTime' : string(startTime)}})
+
+def close_ticket(ticketID):
+    print(ticketID)
+    response = db.tickets.find_one_and_update({'ticketID' : int(ticketID)}, {'$set' : {'status' : "closed"}})
+
 ## Account Functions
+def new_account(accID, username, password, fName, lName):
+    acc_doc = {'accID' : int(accID), 'username' : username, 'password' : password, 'fName' : fName, 'lName' : lName}
+    return db.accounts.insert_one(acc_doc)
+
+def delete_account(accID):
+    db.accounts.delete_one({'accID' : accID})
+
+def get_account_count():
+    return db.accounts.count_documents({})
+
+def get_accounts():
+    return db.accounts.find()
